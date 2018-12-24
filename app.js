6958a94aea271ece4d9e1ee46a2a9444dcab2476
Tim <tjk315@gmail.com>
2018-12-24 18:19:11 -0500

    Create initial "UI" Class

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ class Book {
 }
 
 // UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    // Hard-coded StoredBooks array for demo (Acts as local storage)
+    const StoredBooks = [
+      {
+        title: 'Book One',
+        author: 'John Doe',
+        isbn: '3434434'
+      },
+      {
+        title: 'Book Two',
+        author: 'Jane Doe',
+        isbn: '45545'
+      }
+    ];
+
+    const book = StoredBooks;
+
+  }
+}
 
 // Store Class: Handles Storage
 
