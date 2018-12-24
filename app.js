b4cfa51d5011e514a6d1ba35293b6c342cc3572b
Tim <tjk315@gmail.com>
2018-12-24 18:14:23 -0500

    Create "Book" Class

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
 
 // UI Class: Handle UI Tasks
 
